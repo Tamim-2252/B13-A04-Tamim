21bf17373cd0b7204ee52f0bb85aa50e5884de6c
amim-2252 <tamimahmed0251@gmail.com>
2026-03-02 00:16:39 +0600

    added card function

--- script/script.js
+++ script/script.js
@@ -1,32 +1,93 @@
-let total = document.getElementById('total');
-let stotal = document.getElementById('stotal');
-let totalInterview = document.getElementById('total-interview');
-let totalRejected = document.getElementById('total-rejected')
+const total = document.getElementById('total');
+const available = document.getElementById('available');
+const totalInterview = document.getElementById('total-interview');
+const totalRejected = document.getElementById('total-rejected')
 
-const jobContainer = document.getElementById('job-container');
 const tabActive = ["bg-[#3B82F6]", "text-white", "font-semibold"]
-const tabInactive = ["bg-white", "border", "border-[#F1F2F4]" ,"text-[gray]", "font-medium",]
+const tabInactive = ["bg-white", "border", "border-[#F1F2F4]", "text-[gray]", "font-medium",]
 
-
-// count function
- function countingJobs () {
-    total.innerText = jobContainer.children.length;
-    stotal.innerText = jobContainer.children.length;
-} countingJobs()
+const allContainer = document.getElementById('all-container');
+const interviewContainer = document.getElementById('interview-container');
+const rejectedContainer = document.getElementById('rejected-container');
+const noJob = document.getElementById("no-job");
+// let nameC = allContainer;
 
 // button function
-function switchTab(tab) {
-    const tabs = ["all","interview","rejected"]
 
+function switchTab(tab) {
+    const tabs = ["all", "interview", "rejected"]
+    // nameC = tab;
     for (const t of tabs) {
         const tabName = document.getElementById(t);
         if (t === tab) {
             tabName.classList.remove(...tabInactive);
             tabName.classList.add(...tabActive);
         }
-        else{
+        else {
             tabName.classList.remove(...tabActive);
-            tabName.classList.add(...tabInactive);            
+            tabName.classList.add(...tabInactive);
         }
-    }
+
+        const containerName = document.getElementById(t + "-container")
+        if (t === tab) {
+            containerName.classList.remove("hidden");
+            if (containerName.children.length < 1) {
+                noJob.classList.remove("hidden");
+            }
+            else {
+                noJob.classList.add("hidden");
+            }
+        }
+        else {
+            containerName.classList.add("hidden");
+        }
+    }countingJobs()
 }
+
+// count function
+function countingJobs() {
+
+    total.innerText = allContainer.children.length;
+    totalInterview.innerText = interviewContainer.children.length;
+    totalRejected.innerText = rejectedContainer.children.length;
+    available.innerText = allContainer.children.length;
+
+    if (total.innerText < 1) {
+        noJob.classList.remove("hidden");
+    } 
+    else if (totalInterview.innerText < 1) {
+        noJob.classList.remove("hidden");
+    }
+    else if (totalRejected.innerText < 1) {
+        noJob.classList.remove("hidden");
+    } 
+    else {
+        noJob.classList.add("hidden");
+    }
+} countingJobs()
+
+// card function
+
+document.getElementById("jobs-container").addEventListener("click", function (event) {
+    const clickedElement = event.target;
+    const card = clickedElement.closest(".card");
+    const states = card.querySelector(".states");
+    const parent = card.parentNode;
+
+    if (clickedElement.classList.contains("interview")) {
+        interviewContainer.appendChild(card);
+        states.innerText = "INTERVIEW";
+        states.classList.add("text-[#10B981]");
+        countingJobs();
+    }
+    if (clickedElement.classList.contains("rejected")) {
+        rejectedContainer.appendChild(card);
+        states.innerText = "REJECTED";
+        states.classList.add("text-[red]");
+        countingJobs();
+    }
+    if (clickedElement.classList.contains("delete")) {
+        parent.removeChild(card);
+        countingJobs();
+    }
+})
